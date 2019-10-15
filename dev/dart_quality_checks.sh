110803c40b75e725977cccb9c92e6393479f715b
Alexei Sintotski <alexei.sintotski@gmail.com>
2019-10-15 08:28:46 +0200

    Smoke test

--- dev/dart_quality_checks.sh
+++ dev/dart_quality_checks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 dartanalyzer --fatal-hints .
+pub run test
 pub run dependency_validator --ignore=functional_data_generator,sum_types_generator
 dev/format_dart_code.sh --set-exit-if-changed
 
