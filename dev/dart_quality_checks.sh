a1e00c9d09ea0c2d1f87031daa3cf631adde80a8
Alexei Sintotski <alexei.sintotski@gmail.com>
2019-10-16 19:47:13 +0200

    Codecov integration

--- dev/dart_quality_checks.sh
+++ dev/dart_quality_checks.sh
@@ -2,7 +2,7 @@
 
 dartanalyzer --fatal-hints .
 pub run test
-pub run dependency_validator --ignore=functional_data_generator,sum_types_generator
+pub run dependency_validator --ignore=functional_data_generator,sum_types_generator,test_coverage
 dev/format_dart_code.sh --set-exit-if-changed
 
 dev/generate_code.sh --delete-conflicting-outputs
