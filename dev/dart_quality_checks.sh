37900176d2e5d6de0eada2d03a9a4d3d0e1494f0
Alexei Sintotski <alexei.sintotski@gmail.com>
2021-12-27 10:16:32 +0100

    Dev scripts use dart tool

--- dev/dart_quality_checks.sh
+++ dev/dart_quality_checks.sh
@@ -3,8 +3,8 @@
 dart example/main.dart
 
 dartanalyzer --fatal-hints .
-pub run test
-pub run dependency_validator
+dart run test
+dart run dependency_validator
 dev/format_dart_code.sh --set-exit-if-changed
 
 dev/generate_code.sh --delete-conflicting-outputs
@@ -12,4 +12,4 @@ if [ -n "$(git status --porcelain -- **/*.g.dart)" ]; then
   exit 1
 fi
 
-pub publish --dry-run
+dart pub publish --dry-run
