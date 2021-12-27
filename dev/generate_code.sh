e915fbd756cae46193998414f88c3e082d5f14d6
Alexei Sintotski <alexei.sintotski@gmail.com>
2021-12-27 10:16:32 +0100

    Dev scripts use dart tool

--- dev/generate_code.sh
+++ dev/generate_code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 find . -name '*.g.dart' | xargs rm -f
-pub run build_runner build "$@"
+dart run build_runner build "$@"
 dev/format_dart_code.sh
 find . -name '*.g.dart' | xargs -I % sh -c 'cat dev/ignored_lint_warning_for_generated_code.txt >> %'
